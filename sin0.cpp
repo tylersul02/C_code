9983507b86a4e8a03a4cc6445ce8c399b723c655
tylersul02 <42984907+tylersul02@users.noreply.github.com>
2019-04-09 10:01:52 -0700

    Add files via upload

---
+++ sin0.cpp
@@ -0,0 +1,15 @@
+// g++ sin0.cpp -o sin0.o
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+double PI=3.14159265358979323846;
+double rad = 0;
+double t, result=0; // theta
+	for ( t=0 ; t<=360 ; t = t + 15 )
+	{
+		result = sin(t);
+			cout <<t<<"\t"<< result<<endl;
+	}
+	return 0;
+}
---
+++ sinperamp.cpp
@@ -0,0 +1,38 @@
+//sinperamp.cpp
+//g++ sinperamp.cpp -o sinperamp.o
+#include <iostream>
+#include <cmath>
+using namespace std;
+double round4(double var)
+{
+	double value;
+	 if (var < 0 ){
+		  value = (int)(var * 10000 - 0.00005);
+	  }
+	 else{
+		  value = (int)(var * 10000 + 0.00005);
+		 }
+		return (double)value /10000;
+}
+int main (){
+double PI=3.14159265359;
+double a, p, t, rad, sr, aspr;
+//t(theta) rad(radian) sr(sine result)
+//aspr (amplitude * sine (period*t))
+	cout<<"\n Input a Amplitude : ";
+	cin>>a;
+	cout<<"\n Input a Period : ";
+	cin>>p;
+	cout <<"Theta\tsin(t)\ta*sin(pt) \n";
+	for ( t=0 ; t<=360 ; t = t + 15 ) 
+	{
+	  rad = t * (PI / 180);
+	  sr = sin(rad);
+	  aspr = a*sin(p*t);
+	  cout<<t<<"\t"<<sr<<"\t"<<aspr<<"\n";
+	
+	}
+	return 0;
+    
+ 
+}
